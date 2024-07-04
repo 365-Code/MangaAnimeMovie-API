fd1072176df50d81839d0ad01577242deb4f0111
365-Code <sohailahmedbhati786@gmail.com>
2024-07-04 12:36:53 +0530

    some changes in api

--- src/app/api/manga/anilist/manga-info/route.ts
+++ src/app/api/manga/anilist/manga-info/route.ts
@@ -1,17 +1,37 @@
-import { META } from "@consumet/extensions";
-import { NextRequest, NextResponse } from "next/server";
+// import { META } from "@consumet/extensions";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(req: NextRequest) {
+//   try {
+//     const anilist = new META.Anilist.Manga();
+//     const { searchParams } = new URL(req.url);
+//     const id = searchParams.get("id") || "";
+//     const results = await anilist.fetchMangaInfo(id)
+//     return NextResponse.json({ data: results }, { status: 200 });
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
+
+
+// pages/api/example.js
+
+import { META } from '@consumet/extensions';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   try {
     const anilist = new META.Anilist.Manga();
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id") || "";
-    const results = await anilist.fetchMangaInfo(id, {
-      "Access-Control-Allow-Origin": "*" ,
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    })
-    return NextResponse.json({ data: results }, { status: 200 });
+    const results = await anilist.fetchMangaInfo(id);
+
+    const response = NextResponse.json({ data: results }, { status: 200 });
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+
+    return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
