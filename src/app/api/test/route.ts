c85d10819fa0ebda1b862f09ba8f4b816c9c243a
365-Code <sohailahmedbhati786@gmail.com>
2024-07-04 12:54:39 +0530

    test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,19 +1,18 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  // Using the example id of "30013" and the query of "mangareader"
-  const url = "https://api.consumet.org/meta/anilist-manga/info/152128";
   try {
-    console.log(url + new URLSearchParams({provider: "mangareader"}));
-    
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id") || "";
     const response = await fetch(
-      url + new URLSearchParams({ provider: "mangareader" })
+      `${process.env.mangaURI}?id=${id}`
     );
-    const data = response;
-    // const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (err: any) {
-    throw new Error(err.message);
+
+    const { data } = await response.json();
+    return NextResponse.json({
+      data,
+    });
+  } catch (error) {
+    return NextResponse.error();
   }
 }
