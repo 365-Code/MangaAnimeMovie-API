b5130d5b9f22591a7ff5c25525dca1b0d8ba21cb
365-Code <sohailahmedbhati786@gmail.com>
2024-07-04 12:31:56 +0530

    testing something

--- src/app/api/manga/anilist/manga-info/route.ts
+++ src/app/api/manga/anilist/manga-info/route.ts
@@ -6,7 +6,11 @@ export async function GET(req: NextRequest) {
     const anilist = new META.Anilist.Manga();
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id") || "";
-    const results = await anilist.fetchMangaInfo(id, "mangareader");
+    const results = await anilist.fetchMangaInfo(id, {
+      "Access-Control-Allow-Origin": "*" ,
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    })
     return NextResponse.json({ data: results }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
